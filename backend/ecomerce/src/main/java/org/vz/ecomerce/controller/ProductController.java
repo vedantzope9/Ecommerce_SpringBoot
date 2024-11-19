d8302cd43439718c3292ff21afaf6aa86f9d279a
Vedant Zope <vedantzope9@gmail.com>
2024-11-20 00:35:59 +0530

    Backend setup

---
+++ backend/ecomerce/src/main/java/org/vz/ecomerce/controller/ProductController.java
@@ -0,0 +1,14 @@
+package org.vz.ecomerce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello";
+    }
+}
---
+++ backend/ecomerce/src/main/java/org/vz/ecomerce/model/Product.java
@@ -0,0 +1,27 @@
+package org.vz.ecomerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+}
